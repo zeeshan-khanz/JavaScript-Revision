0c3e8885a2c10d9164fd47d29745fdaeee286c50
zeeshan-khanz <164619721+zeeshan-khanz@users.noreply.github.com>
2025-10-29 21:32:46 +0500

    Create Student Marks Tracker.js

---
+++ Student Marks Tracker.js
@@ -0,0 +1,6 @@
+console.log("Student Marks Tracker")
+let student=["Zeeshan","Hassan","Abdullah"]
+let marks=[90,80,60]
+for(let i=0; i<student.length; i++){
+    console.log(`${student[i]} scored ${marks[i]} marks.`)
+}
